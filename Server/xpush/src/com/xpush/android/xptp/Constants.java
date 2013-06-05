05fa736bb55733fd029ab74fa24fea0b69978652
hugoxian <hugo.xian@qq.com>
2013-06-05 11:46:21 +0800

    annotation

--- Server/xpush/src/com/xpush/android/xptp/Constants.java
+++ Server/xpush/src/com/xpush/android/xptp/Constants.java
@@ -6,13 +6,14 @@ package com.xpush.android.xptp;
  * 
  */
 public interface Constants {
-	public String TYPE_REGISTER="REGISTER";
+	// 各种数据包的标示
+	public String TYPE_REGISTER = "REGISTER";
 	public String TYPE_LOGIN = "LOGIN";
 	public String TYPE_HEARTBEAT = "HEARTBEAT";
 	public String TYPE_MESSAGE = "MESSAGE";
 	public String TYPE_RESULT = "RESULT";
-	
+
 	public String APP_KEY = "APP_KEY";
 	public String PHONE_DEVICEID = "DEVICE_ID";
-	
+
 }
